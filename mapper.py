a96b082d15da22534b15b9f5eac86ee93b51df18
calvin99999 <calvin_emmenthal@seneca3.lehre.hwr-berlin.de>
2020-06-08 20:39:36 +0200

    Exercise3

--- mapper.py
+++ mapper.py
@@ -34,8 +34,11 @@ for line in sys.stdin:
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
 
-    # store the 6 elements of the tuple in seperate variables
-    date, time, item, category, sales, payment = data
+    if len(data)==6:# store the 6 elements of the tuple in seperate variables
+    	date, time, item, category, sales, payment = data
+    else:
+	raise Exception('Not 6 elements')
+		
 
     # Write the key-value combination to standard output (stdout)
     # Key is the payment, value is the sales     
